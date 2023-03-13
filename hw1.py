d89b1bec8f19a58174aa3d480949b1e2320c04ec
junyong838 <127418540+junyong838@users.noreply.github.com>
2023-03-13 15:12:55 +0900

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -1,1 +1,9 @@
-print('hellow world')
+def get_radius(prompt):
+    r = int(input(prompt))
+    return r
+def get_circle_area(r):
+    return 3.14*r*r
+prompt = "넓이를 구하고자 하는 원의 반지름은?"
+r = get_radius(prompt)
+a = get_circle_area(r)
+print("반지름 {0}인 원의 넓이 = 3.14 x {0} x {0} = {1}".format(r, a))
