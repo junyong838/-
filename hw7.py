0e77efe2d3447786e6d184586b20bbb17a96495d
junyong838 <127418540+junyong838@users.noreply.github.com>
2023-04-28 15:44:01 +0900

    Add files via upload

--- hw7.py
+++ hw7.py
@@ -7,7 +7,7 @@ while True:
         break
     c = int(input('수량은?'))
     shopping_bag[item] = c
-    print(f'장바구니에 {item}이(가) 담겼습니다.')
+    print(f'장바구니에 {item} {c}개 담겼습니다.')
 print(f'>>> 장바구니 보기:{shopping_bag}')
 print('[검색]')
 sr = input('장바구니에서 확인하고자 하는 상품은?')
